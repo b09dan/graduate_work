07459d90a225f75a0f46acd0aaf31e9aa5c0ee1c
Bogdan <i@bogdan.co>
2016-12-01 02:47:11 +0300

    Собрана статистика тональностей по статьям с АПРЕЛЯ. Подготовка к построению прогноза по имеющеся статистике.

--- code/parsing_news.py
+++ code/parsing_news.py
@@ -1,42 +1,43 @@
-def parsing_news(site, category_path):
-    from urllib.request import urlopen  # Library for urlopen
-    from bs4 import BeautifulSoup  # Library for html parser (scraper), lxml is also nice
-    numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven",
-               "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
-               "twentyone", "twentytwo", "twentythree", "twentyfour", "twentyfive", "twentysix", "twentyseven",
-               "twentyeight", "twentynine", "thirty", "thirtyone", "thirtytwo", "thirtythree", "thirtyfour",
-               "thirtyfive", "thirtysix", "thirtyseven", "thirtyeight", "thirtynine", "forty", "fortyone", "fortytwo",
-               "fortythree", "fortyfour", "fortyfive", "fortysix", "fortyseven", "fortyeight", "fortynine", "fifty",
-               "fiftyone", "fiftytwo", "fiftythree", "fiftyfour", "fiftyfive", "fiftysix", "fiftyseven", "fiftyeight",
-               "fiftynine", "sixty", "sixtyone", "sixtytwo", "sixtythree", "sixtyfour", "sixtyfive", "sixtysix",
-               "sixtyseven", "sixtyeight", "sixtynine", "seventy", "seventyone", "seventytwo", "seventythree",
-               "seventyfour", "seventyfive", "seventysix", "seventyseven", "seventyeight", "seventynine", "eighty",
-               "eightyone", "eightytwo", "eightythree", "eightyfour", "eightyfive", "eightysix", "eightyseven",
-               "eightyeight", "eightynine", "ninety", "ninetyone", "ninetytwo", "ninetythree", "ninetyfour",
-               "ninetyfive", "ninetysix", "ninetyseven", "ninetyeight", "ninetynine",
-               "hundred", ]  # Array for numerating articles
+from urllib.request import urlopen  # Library for urlopen
+from bs4 import BeautifulSoup  # Library for html parser (scraper), lxml is also nice
+from code.stemming_article import stemming_article
+import datetime
+import time
+import json
 
-    wsj_econ = urlopen(site + '/' + category_path).read().decode('utf-8',
-                                                                 'ignore')  # Making url, reading web-page and converting it to utf-8
-    wsj_econ_source = BeautifulSoup(wsj_econ, "html.parser")  # Converting to BS type
-    wsj_econ_source_urls = BeautifulSoup(str(wsj_econ_source.find_all('div', class_="media__image")),
-                                         "html.parser")  # finding all pics with news link
-    final_links = []  # Creating empty array for links
-    for link in wsj_econ_source_urls.find_all('a'):  # Through loop searching for links
-        final_links.append(site + link.get('href'))  # Putting them to the array
-    i = 0  # Creating an empty variable for an increment to dot all articles' numbers
-    final_articles = []  # Creating empty array for articles
-    for url_article in final_links:  # Through loop searching for articles' text
-        pre_source_article = urlopen(url_article).read().decode('utf-8', 'ignore')  # Reading pages
-        source_article = BeautifulSoup(pre_source_article, "html.parser")  # Converting to BS type
-        text_article = BeautifulSoup(str(source_article.find_all('div', class_="article__text text ")),
-                                     "html.parser")  # searching for articles' text
-        final_articles.append('Article number ' + numbers[i] + ':\n' + text_article.text)  # putting it to array
-        i += 1;
-        print('.')  # loading string
-        print('Downloading File FooFile.txt [%d%%]\r' % i, end="")
-    return final_articles
+site = 'https://www.rt.com'
+category_path='business'
 
-# from parsing_news import parsing_news
-# a=parsing_news('https://www.rt.com','business')
-# print(a)
+#wsj_econ = urlopen(site + '/' + category_path).read().decode('utf-8', 'ignore')  # Making url, reading web-page and converting it to utf-8
+wsj_econ = urlopen('http://bogdan.co/BusinessRT.html').read().decode('utf-8', 'ignore')  # Making url, reading web-page and converting it to utf-8
+wsj_econ_source = BeautifulSoup(wsj_econ, "html.parser")  # Converting to BS type
+wsj_econ_source_urls = BeautifulSoup(str(wsj_econ_source.find_all('div', class_="media__image")), "html.parser")  # finding all pics with news link
+final_links = []  # Creating empty array for links
+for link in wsj_econ_source_urls.find_all('a'):  # Through loop searching for links
+    final_links.append(site + link.get('href'))  # Putting them to the array
+i = 0  # Creating an empty variable for an increment to dot all articles' numbers
+final_articles = {'articles_data': []}  # Creating empty JSON array for articles
+for url_article in final_links:  # Through loop searching for articles' text
+    pre_source_article = urlopen(url_article).read().decode('utf-8', 'ignore')  # Reading pages
+    source_article = BeautifulSoup(pre_source_article, "html.parser")  # Converting to BS type
+    text_article = BeautifulSoup(str(source_article.find_all('div', class_="article__text text ")),
+                                 "html.parser")  # searching for articles' text
+    temp_article = text_article.text
+    temp_published_time = BeautifulSoup(str(source_article.find_all('time', class_="date date_article-header", limit=1)), "html.parser").text
+    for ch in ['\'', '\n','\\n', '  ', '\\', '\\', '"', '’', '‘', '“', '”', ']', '[', '/']:
+        if ch in temp_article:
+            temp_article = temp_article.replace(ch, '')
+            temp_published_time = temp_published_time.replace(ch, '')
+    temp_article = temp_article.replace(r'\\', r'   ')
+    final_articles['articles_data'].append({
+        'article_number': i,
+         # 'article_time_creation': datetime.datetime.utcnow().isoformat(),
+        'published_time' : temp_published_time.replace('Published time: ', ''),
+        'source_text': temp_article,
+        'stemmed_text': stemming_article(temp_article),
+        'url': url_article
+    })
+    print('.')  # loading string
+    i += 1
+with open("/home/bogdan/PycharmProjects/graduate_work/data/news2/"+ time.strftime("%d_%m_%Y_%H_%M") + ".json", "w") as text_file:
+    print(json.dumps(final_articles), file=text_file)
--- code/sentimental_analysis_naive_bayes.py
+++ code/sentimental_analysis_naive_bayes.py
@@ -2,43 +2,37 @@
 # lib url: http://textblob.readthedocs.org/en/dev/api_reference.html#module-textblob.en.sentiments
 # also some detailed tutorial for Bayes classification: http://andybromberg.com/sentiment-analysis-python/#fnref:1
 from os import walk
+import json
+# For graphics
+import numpy as np
+import matplotlib.pyplot as plt
 
 from textblob import TextBlob
 
-from xml_2_array import xml_2_array
-
-dir = input('Enter files directory: ')
+dir = "/home/bogdan/PycharmProjects/graduate_work/data/news2/"
 report_name = input('Please, enter report name: ')
 files_with_news = []
 for (dirpath, dirnames, filenames) in walk(dir):
     files_with_news.extend(filenames)
     break
 polarities_4_plot = []
-with open("news_analysis/"+report_name+".csv", "w", encoding='utf-8') as myfile:
-    myfile.write('day;article_number;polarity\n')
-    for news_day in files_with_news:
-        wiki = xml_2_array(dir.replace('news/', '') +news_day)
-        for i in range(0, len(wiki)):
-            wiki[i] = ' '.join(wiki[i])
-        # print(news_day.replace('.xml', ''))
-        for i in range(0, len(wiki)):
-            wiki[i] = TextBlob(wiki[i])
-            # print('Article №', i, ': ', wiki[i].sentiment)
-            myfile.write(news_day.replace('.xml', '') + ';' +
-                         'article_' + str(i + 1) + ';' +
-                         str(wiki[i].sentiment[0]) + '\n'
-                         )
-            polarities_4_plot.append(wiki[i].sentiment[0])
-print(polarities_4_plot)
-import numpy as np
-import matplotlib.pyplot as plt
+with open("/home/bogdan/PycharmProjects/graduate_work/data/news_analysis/"+report_name+".csv", "w", encoding='utf-8') as myfile:
+    myfile.write('date_time_utc_iso;polarity;url\n')
+    for news_day_file in files_with_news:
+        with open(dir+news_day_file) as json_file:
+            json_file = json.load(json_file)
+            for article_in_file in json_file['articles_data']:
+                temp_sentiment = TextBlob(article_in_file['source_text']).sentiment[0]
+                myfile.write(article_in_file['published_time'] + ';' + str(temp_sentiment) + ';' + article_in_file['url']+ '\n')
+                polarities_4_plot.append(temp_sentiment)
 
 y = np.asarray(polarities_4_plot)
-x = np.arange(0, len(y), 1);
+x = np.arange(0, len(y), 1)
 
 plt.axis([0.0,len(y)+1, min(y)-0.01,max(y)+0.01])
 ax = plt.gca()
 ax.set_autoscale_on(False)
 plt.plot(x, y)
-plt.legend(['Probability distribution'])
+plt.legend(['Тональности новостей'])
 plt.show()
+
